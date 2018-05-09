1f1691a233308c31f0c0e2dd5c6b33ccce69d574
Leon Zhou <lzhou95@gmail.com>
2018-05-08 20:56:12 -0700

    implement basic classification models; prior to test run

---
+++ bin/modeling/gbc.py
@@ -0,0 +1,31 @@
+import logging
+import os
+
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.externals import joblib
+
+from . import tfidf
+
+
+def train_gbc(df, column, size=0.3, state=42):
+    filepath = '../data/models/gbc_' + column + '.pkl'
+    Xtr, Xte, Ytr, Yte = tfidf.generate_tfidf_split(df, column, size, state)
+
+    clf = GradientBoostingClassifier(max_depth=10)
+
+    logging.debug('train_gbc(): Training model')
+    clf.fit(Xtr, Ytr)
+
+    logging.debug("train_gbc(): Dumping GradientBoostingClassifier to '" + filepath + "'")
+    joblib.dump(clf, filepath)
+    return clf, Xte, Yte
+
+
+def get_gbc(suffix):
+    filepath = '../data/models/gbc_' + suffix + '.pkl'
+    if os.path.isfile(filepath):
+        logging.debug('get_gbc(): Loading saved classifier.')
+        return joblib.load(filepath)
+    else:
+        logging.error('get_gbc(): Saved classifier not found.')
+        return None
---
+++ bin/modeling/logistic.py
@@ -0,0 +1,36 @@
+import logging
+import os
+
+from sklearn.externals import joblib
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import GridSearchCV
+
+from . import tfidf
+
+
+def train_logistic(df, column, size=0.3, state=42):
+    filepath = '../data/models/logistic_' + column + '.pkl'
+    Xtr, Xte, Ytr, Yte = tfidf.generate_tfidf_split(df, column, size, state)
+
+    params = {'C': [1e-3, 1e-2, 1e-1, 1, 1e1, 1e2, 1e3], 'multi_class': ['ovr', 'multinomial'],
+              'solver': ['newton-cg', 'sag', 'saga', 'lbfgs']}
+    clf = GridSearchCV(LogisticRegression(), param_grid=params)
+    logging.debug('train_logistic(): Training model')
+    clf.fit(Xtr, Ytr)
+
+    logging.debug('train_logistic(): Best parameters for column : ' + column)
+    logging.debug(clf.best_params_)
+
+    logging.debug("train_logistic(): Dumping LogisticRegression to '" + filepath + "'")
+    joblib.dump(clf.best_estimator_, filepath)
+    return clf, Xte, Yte
+
+
+def get_logistic(suffix):
+    filepath = '../data/models/logistic_' + suffix + '.pkl'
+    if os.path.isfile(filepath):
+        logging.debug('get_logistic(): Loading saved classifier.')
+        return joblib.load(filepath)
+    else:
+        logging.error('get_logistic(): Saved classifier not found.')
+        return None
